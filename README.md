5a2e07eda7abb992055f09a467f1fabc79996112
7sDream <i@7sdre.am>
2021-02-07 22:47:07 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,28 @@
+# 玲儿
+
+自用 QQ 机器人。
+
+## 功能
+
+- Bilibili 用户监控，参见：[nonebot-plugin-bam]
+
+## 使用
+
+```bash
+pip install qqbot-linger
+cd /a/path/you/want/store/config/and/database
+vim .env # edit this file, see `.env.sample file` for a example
+
+# Start cqhttp server
+# like go-cqhttp, cqhttp-mirai etc
+
+linger # start the bot
+```
+
+## 截图
+
+以后再弄。
+
+## LICENSE
+
+Unlicense.
---
+++ qqbot_linger/main.py
@@ -0,0 +1,19 @@
+import nonebot
+from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+
+
+def main():
+    nonebot.init(_env_file=".env")
+
+    driver = nonebot.get_driver()
+    driver.register_adapter("cqhttp", CQHTTPBot)
+
+    nonebot.load_builtin_plugins()
+    nonebot.load_plugin("nonebot_plugin_apscheduler")
+    nonebot.load_plugin("nonebot_plugin_bam")
+
+    nonebot.run()
+
+
+if __name__ == "__main__":
+    main()
