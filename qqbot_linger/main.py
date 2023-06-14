c580fa91fc8ce62fd4eed471a5c3db549e6a33fe
7sDream <i@7sdre.am>
2023-06-15 02:49:58 +0800

    feat: update to nonebot 2.0.0 stable ver

--- qqbot_linger/main.py
+++ qqbot_linger/main.py
@@ -1,17 +1,17 @@
 import nonebot
-from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+from nonebot.adapters.onebot.v11 import Adapter
 
 
 def main():
     nonebot.init(_env_file=".env")
 
     driver = nonebot.get_driver()
-    driver.register_adapter("cqhttp", CQHTTPBot)
+    driver.register_adapter(Adapter)
 
     nonebot.load_builtin_plugins()
     nonebot.load_plugin("nonebot_plugin_apscheduler")
     nonebot.load_plugin("nonebot_plugin_bam")
-    nonebot.load_plugin("nonebot_plugin_7s_roll")
+    # nonebot.load_plugin("nonebot_plugin_7s_roll")
 
     nonebot.run()
 
